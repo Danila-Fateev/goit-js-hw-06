8ddffa35cbf44d5c18fe229cef05e7ecd4c1134d
Вадим Фатеев <fateev1vv2@gmail.com>
2022-03-20 14:02:31 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+ingredients.forEach(el => {
+  const newEl = document.createElement("li");
+  newEl.textContent = el;
+  newEl.classList.add("item");
+  ingredientsEl.append(newEl)
+})
