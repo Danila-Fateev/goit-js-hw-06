58a578457c4c6a760e3f4fae69dc85ad29582bc8
Вадим Фатеев <fateev1vv2@gmail.com>
2022-03-24 18:02:19 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanEl = document.querySelector('span.color');
+
+const buttonEl = document.querySelector('button.change-color');
+buttonEl.addEventListener('click', () => {
+  const bgColor = getRandomHexColor();
+  
+  document.body.style.backgroundColor = bgColor;
+  spanEl.textContent = bgColor;
+})
