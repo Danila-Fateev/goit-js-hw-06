0dd02f9c715f04e2c640368387dd442f92fc529a
Вадим Фатеев <fateev1vv2@gmail.com>
2022-03-24 14:48:12 +0200

    task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,10 +1,13 @@
-let counterValue = ;
-
-
-
-document.querySelector("[data-action]").addEventListener("click", () => counterValue -= 1)
-
-console.log(counterValue);
+let counterValue = 0;
 
 const spanEl = document.querySelector("#value");
-spanEl.textContent = counterValue;
+
+document.querySelector('button[data-action="decrement"]').addEventListener("click", () => {
+    counterValue -= 1;
+    spanEl.textContent = counterValue;
+})
+
+document.querySelector('button[data-action="increment"]').addEventListener("click", () => {
+    counterValue += 1;
+    spanEl.textContent = counterValue;
+})
