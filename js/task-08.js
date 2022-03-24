ba2a7a8f6c4d9c0700292a1802ce9c6df63dac65
Вадим Фатеев <fateev1vv2@gmail.com>
2022-03-24 17:55:19 +0200

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const formElement = document.querySelector('form.login-form');
+    
+    
+    formElement.addEventListener('submit', (event) => {
+    event.preventDefault();
 
+    const {
+     elements: { email, password }
+    } = event.currentTarget;
+    
+        
+    if (email.value === '' && password.value === '') {
+        return alert('Заполните поля');     
+    } if (email.value === '') {
+       return alert('Заполните поле почты')
+    }if (password.value === '') {
+       return alert('Заполните поле пароля')
+    }
+    
+    const userData = {
+        email: email.value,
+        password: password.value,
+    }
+
+    console.log(userData)
+
+    formElement.reset();
+});
