7cd632173cfd066cfd3cf6c9e03676f14f815828
Вадим Фатеев <fateev1vv2@gmail.com>
2022-03-26 13:16:03 +0200

    TASK-02 corrected

--- js/task-02.js
+++ js/task-02.js
@@ -8,10 +8,24 @@ const ingredients = [
 ];
 
 const ingredientsEl = document.querySelector("#ingredients");
+const elements = [];
 
 ingredients.forEach(el => {
   const newEl = document.createElement("li");
-  newEl.textContent = el;
+      newEl.textContent = el;
   newEl.classList.add("item");
-  ingredientsEl.append(newEl)
+    elements.push(newEl);
 })
+
+ingredientsEl.append(...elements);
+
+// function addItemsToList(array) {
+//   for (let i = 0; i < array.length; i += 1) {
+//     const newEl = document.createElement("li");
+//       newEl.textContent = array[i];
+//   newEl.classList.add("item");
+//     elements.push(newEl);
+//   }
+//   ingredientsEl.append(...elements);
+// }
+// addItemsToList(ingredients);
